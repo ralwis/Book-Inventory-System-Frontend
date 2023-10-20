b14175c6f828874e46d7c521e0a7ec115012a6a5
ravindu <ravindurcg99@yahoo.com>
2023-10-20 19:49:42 +0530

    Add New Book

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,6 +4,12 @@ import axios from 'axios';
 
 function Home() {
     const [books, setBooks] = useState([]);
+    const [newBook, setNewBook] = useState({
+        title: '',
+        author: '',
+        isbn: '',
+        publicationDate: ''
+    });
 
     const apiBaseUrl = 'http://localhost:5029/api/Books';
 
@@ -19,6 +25,16 @@ function Home() {
     useEffect(() => {
         getAllBooks();
     }, []);
+
+    const addBook = async () => {
+        try {
+          await axios.post(apiBaseUrl, newBook);
+          setNewBook({});
+          getAllBooks();
+        } catch (error) {
+          console.error(error);
+        }
+    };
     
   return (
     <div>
@@ -30,6 +46,13 @@ function Home() {
             </li>
             ))}
         </ul>
+
+        <h2>Add New Book</h2>
+        <input type="text" placeholder="Title" value={newBook.title || ''} onChange={(e) => setNewBook({ ...newBook, title: e.target.value })}/>
+        <input type="text" placeholder="Author" value={newBook.author || ''} onChange={(e) => setNewBook({ ...newBook, author: e.target.value })}/>
+        <input type="text" placeholder="ISBN" value={newBook.isbn || ''} onChange={(e) => setNewBook({ ...newBook, isbn: e.target.value })}/>
+        <input type="date" placeholder="Publication Date" value={newBook.publicationDate || ''} onChange={(e) => setNewBook({ ...newBook, publicationDate: e.target.value })}/>
+        <button onClick={addBook}>Add Book</button>
     </div>
   )
 }

3ed919fd7f6b728c5f756501b594c7f75c7267eb
ravindu <ravindurcg99@yahoo.com>
2023-10-26 03:03:33 +0530

    Add Cards for books and shelves

--- src/Components/DashBoard.jsx
+++ src/Components/DashBoard.jsx
@@ -53,9 +53,6 @@ function DashBoard(){
         }
     }
 
-    // const apiBaseUrl = 'http://localhost:5029/api/Shelve';
-    // const navigate = useNavigate();
-
     useEffect(() => {
         getShelvesData();
     }, []);
@@ -74,12 +71,21 @@ function DashBoard(){
         navigate('/login');
     }
 
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
     return(
         <div>
             <div className='navbar'>
-                <h2>Books Inventory System</h2>
+                <h1>Books Inventory System</h1>
                 <div>
-                    <button onClick={openAddShelve}>Add New Shelve</button>
+                    <button id='addNewShelve' onClick={openAddShelve}>Add New Shelve</button>
                     <button onClick={logOut}>Logout</button>
                 </div>
             </div>
@@ -87,7 +93,7 @@ function DashBoard(){
             <div className='addNewShelve'>
                 {isAddShelveOpen && (
                     <div className="add-shelve-popup">
-                        <span>Add Shelve</span>
+                        <span id='addShelve'>Add Shelve</span>
                         <input
                             value={shelveName}
                             onChange={(e) => setShelveName(e.target.value)}
@@ -102,10 +108,11 @@ function DashBoard(){
 
             <div className='shelvesContainer'>
                 {shelves.map(({ Id, ShelveName }) => (
-                    <Link to={`/shelve/${Id}`} key={Id}>
+                    <Link to={`/shelve/${Id}`} key={Id} className="shelve-link">
                         <div className='shelveCard'>
-                            <h4>{ShelveName}</h4>
-                            <p>Books: {bookCounts[Id] || 0}</p> {/* Display the book count */}
+                            <div className="shelve-nav" style={{ backgroundColor: getRandomColor() }}></div>
+                            <h2>{ShelveName}</h2>
+                            <h3 className="book-count">Books Count : {bookCounts[Id] || 0}</h3>
                         </div>
                     </Link>
                 ))}
@@ -114,4 +121,4 @@ function DashBoard(){
     )
 }
 
-export default DashBoard
+export default DashBoard;
